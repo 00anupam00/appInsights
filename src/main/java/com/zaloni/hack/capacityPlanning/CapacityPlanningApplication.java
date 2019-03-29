5886c4219d3b48b661cb5419528184b67bb77c5d
arakshit <arakshit@zaloni.com>
2019-03-29 17:41:35 +0530

    Initial Setup

---
+++ src/main/java/com/zaloni/hack/capacityPlanning/CapacityPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.zaloni.hack.capacityPlanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacityPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacityPlanningApplication.class, args);
+	}
+
+}
